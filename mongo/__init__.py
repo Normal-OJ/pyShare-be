98a26e220e0130eeaf3fa2df8f4f0d941096c76c
bogay <pojay11523@gmail.com>
2020-03-27 06:46:49 +0800

    Fix dependency: remove submission module

--- mongo/__init__.py
+++ mongo/__init__.py
@@ -1,19 +1,25 @@
 from . import engine
 from . import user
-from . import submission
+# from . import submission
 from . import problem
 from . import post
+from . import course
+from . import utils
 
 from .engine import *
 from .user import *
-from .submission import *
+# from .submission import *
 from .problem import *
 from .post import *
+from .course import *
+from .utils import *
 
 __all__ = [
     *engine.__all__,
     *user.__all__,
-    *submission.__all__,
+    # *submission.__all__,
     *problem.__all__,
     *post.__all__,
+    *course.__all__,
+    *utils.__all__,
 ]
