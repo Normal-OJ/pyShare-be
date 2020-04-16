7e43ef1f44815f874a833ad0d0f716d62f2dac51
Bogay <pojay11523@gmail.com>
2020-04-17 01:23:13 +0800

    Fix layout

--- mongo/tag.py
+++ mongo/tag.py
@@ -12,18 +12,14 @@ class Tag(MongoBase, engine=engine.Tag):
         '''
         delete the problem
         '''
-
-
-# remove tag from problem if it have
-
-    engine.Problem.objects(tags=self.value).update(pull__tags=self.value)
-    self.obj.delete()
+	# remove tag from problem if it have
+        engine.Problem.objects(tags=self.value).update(pull__tags=self.value)
+        self.obj.delete()
 
     @classmethod
     def add(value):
         '''
-		add a tag to db
-		'''
+        add a tag to db
+        '''
         t = engine.Tag(value)
-
-    t.save()
+        t.save()
