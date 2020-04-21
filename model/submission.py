86a9febbbdda6ab943cc112d5146bdeabdbda115
bogay <pojay11523@gmail.com>
2020-04-21 20:59:57 +0800

    Implement `/submission/<_id>/complete`

---
+++ model/submission.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request
+from mongo import *
+from .utils import *
+
+__all__ = ['submission_api']
+
+submission_api = Blueprint('submission_api', __name__)
+
+
+@submission_api.route('/<_id>/complete', methods=['PUT'])
+def complete(_id):
+    token = request.values['token']
+    if not Token(token).verify(_id):
+        return HTTPError('i don\'t know you :(', 403)
+    submission = Submission(_id)
+    if not submission:
+        return HTTPError('submission not exists!', 404)
+    files = request.files.getlist('files')
+    submission.complete(
+        files,
+        request.values['stderr'],
+        request.values['stdout'],
+    )
+    return HTTPResponse('ok')
