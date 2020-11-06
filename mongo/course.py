9bd9efc1218c836c17d51e9c13932f56e6572243
skps2010 <skps2010030908@yahoo.com>
2020-11-06 23:21:23 +0800

    add missing keys in header

--- mongo/course.py
+++ mongo/course.py
@@ -47,7 +47,7 @@ class Course(MongoBase, engine=engine.Course):
 
     def statistic_file(self):
         f = io.StringIO()
-        statistic_fields = list(User('').statistic().keys())
+        statistic_fields = [*User('').statistic().keys(), 'success', 'fail']
         writer = csv.DictWriter(
             f,
             fieldnames=[
