4ab2aae1797ff15d6dea6bdbfe0ce1cb9dec4855
bogay <pojay11523@gmail.com>
2020-12-01 03:01:48 +0800

    Add missing function (`tests.utils.user.data`)

--- tests/utils/user.py
+++ tests/utils/user.py
@@ -6,6 +6,24 @@ def random_username():
     return secrets.token_hex(8)
 
 
+def data(
+    username=None,
+    password=None,
+    email=None,
+):
+    if username is None:
+        username = random_username()
+    if password is None:
+        password = secrets.token_urlsafe()
+    if email is None:
+        email = f'{secrets.token_hex(8)}@noj.tw'
+    return {
+        'username': username,
+        'password': password,
+        'email': email,
+    }
+
+
 def lazy_signup(_id=None):
     if _id is None:
         _id = random_username()
