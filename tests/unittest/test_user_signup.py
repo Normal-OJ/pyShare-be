be60ec42b8cfb0e549d3cc49dff5fd18514d58e9
bogay <pojay11523@gmail.com>
2020-12-01 02:55:19 +0800

    Add some testcases about email
    currently these will not pass cause the db is kept the previous data,
    there exists dependency between testcases.
    maybe use a fixture to solve this? i am not sure, need to study more
    about pytest doc.

--- tests/unittest/test_user_signup.py
+++ tests/unittest/test_user_signup.py
@@ -1,32 +1,54 @@
 import pytest
 from mongo import *
-from mongo import engine
 from tests import utils
 
 
 def test_normally_signup():
-    _id = utils.user.random_username()
-    utils.user.lazy_signup(_id)
+    utils.user.randomly_add()
 
 
-def test_not_unique():
-    _id = utils.user.random_username()
-    utils.user.lazy_signup(_id)
+@pytest.mark.parametrize(
+    'field, value',
+    [
+        ('username', 'bogay'),
+        ('email', 'bogay@noj.tw'),
+        # password can be the same
+        pytest.param(
+            'password',
+            'A_v3ry_l0Ng_And_Str0NG_passw0rd',
+            marks=pytest.mark.xfail,
+        )
+    ],
+)
+def test_not_unique(field, value):
+    User.signup(**utils.user.data(**{field: value}))
     with pytest.raises(
             NotUniqueError,
             match=r'.*duplicate.*',
     ):
-        utils.user.lazy_signup(_id)
+        User.signup(**utils.user.data(**{field: value}))
 
 
 def test_invalid_email():
-    USER_DATA = {
-        'username': utils.user.random_username(),
-        'password': 'a_very_l0ng_And_sTR04G_pazzw0rd',
-        'email': 'invalid_email',
-    }
     with pytest.raises(
             ValidationError,
             match=r'.*email.*',
     ):
-        User.signup(**USER_DATA)
+        User.signup(**utils.user.data(email='invalid_email'))
+
+
+def test_email_case_sensitivity():
+    User.signup(**utils.user.data(email='bogay@noj.tw'))
+    with pytest.raises(
+            NotUniqueError,
+            match=r'.*duplicate.*',
+    ):
+        User.signup(**utils.user.data(email='BogAy@nOj.tw'))
+
+
+def test_long_username():
+    with pytest.raises(
+            ValidationError,
+            match=r'.*long.*',
+    ):
+        User.signup(**utils.user.data(username='a' * 32))
