812dbcf0855a33d2cb4f485e26b3a7eca4da4356
AlaRduTP <alardutp37259418@gmail.com>
2021-01-29 22:54:20 +0800

    new feat: notification list

---
+++ mongo/notif.py
@@ -0,0 +1,25 @@
+from . import engine
+from .base import MongoBase
+
+__all__ = ['Notif']
+
+class Notif(MongoBase, engine=engine.Notif):
+    types = engine.Notif.Type
+    @classmethod
+    def new(cls, info):
+        notif = cls.engine(info=info)
+        notif.save()
+        return cls(notif.id)
+
+    def read(self):
+        if self.status == engine.Notif.Status.UNREAD:
+            self.update(status=engine.Notif.Status.READ)
+            self.reload()
+
+    def hide(self):
+        if self.status != engine.Notif.Status.HIDDEN:
+            self.update(status=engine.Notif.Status.HIDDEN)
+            self.reload()
+    
+    def to_dict(self):
+        return self.info.to_dict()
