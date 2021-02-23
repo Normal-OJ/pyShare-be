9888d76a96379652cdec3937c6826fe482fd929a
bogay <pojay11523@gmail.com>
2021-02-23 17:12:26 +0800

    Add test utils for creating problem and course

--- tests/utils/course.py
+++ tests/utils/course.py
@@ -4,6 +4,7 @@ from typing import Union
 from mongo import *
 from mongo import engine
 from .utils import none_or
+from . import user
 
 
 def data(
@@ -14,22 +15,22 @@ def data(
     status: int = None,
 ):
     ret = {
-        'name':
-        none_or(name, secrets.token_hex(16)),
-        'teacher':
-        none_or(
-            teacher,
-            engine.User.objects(role__lt=2).first().username,
-        ),
-        'year':
-        none_or(year, random.randint(109, 115)),
-        'semester':
-        none_or(semester, random.randint(1, 2)),
-        'status':
-        none_or(status, engine.CourseStatus.PUBLIC),
+        'name': none_or(name, secrets.token_hex(16)),
+        'year': none_or(year, random.randint(109, 115)),
+        'semester': none_or(semester, random.randint(1, 2)),
+        'status': none_or(status, engine.CourseStatus.PUBLIC),
     }
+    if teacher is not None:
+        ret['teacher'] = teacher
+    else:
+        try:
+            u = engine.User.objects(role__lt=2).first()
+        except DoesNotExist:
+            # TODO: use enum to define role
+            u = user.lazy_signup(role=1)
+        ret['teacher'] = u.pk
     return ret
 
 
-def random_add(**ks):
+def lazy_add(**ks):
     return Course.add(**data(**ks))
