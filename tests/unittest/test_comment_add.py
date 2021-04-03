67b713b37bd6b939480aa852d6851953b033150f
bogay <pojay11523@gmail.com>
2021-04-04 03:27:58 +0800

    Add unit test for liking comment

--- tests/unittest/test_comment_add.py
+++ tests/unittest/test_comment_add.py
@@ -11,3 +11,15 @@ def test_add_comment():
 
 def test_add_reply():
     utils.comment.lazy_add_reply()
+
+
+def test_like_comment():
+    c = utils.comment.lazy_add_comment()
+    # ensure this user has permission to like comment
+    u = utils.course.student(c.problem.course)
+    assert c.obj not in u.likes
+    assert u.obj not in c.liked
+    c.like(user=u)
+    u.reload()
+    assert c.obj in u.likes
+    assert u.obj in c.liked
