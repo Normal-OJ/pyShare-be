f8b52376fa9893f692e95c67eabb79b02b38e38e
bogay <pojay11523@gmail.com>
2021-04-04 15:11:09 +0800

    Add test for comment serialization

--- tests/unittest/test_comment_add.py
+++ tests/unittest/test_comment_add.py
@@ -1,4 +1,7 @@
+from typing import Callable
+import pytest
 from tests import utils
+from mongo import *
 
 
 def setup_function(_):
@@ -13,6 +16,9 @@ def test_add_reply():
     utils.comment.lazy_add_reply()
 
 
+# TODO: put these functions not related to add to right location
+
+
 def test_like_comment():
     c = utils.comment.lazy_add_comment()
     # ensure this user has permission to like comment
@@ -23,3 +29,27 @@ def test_like_comment():
     u.reload()
     assert c.obj in u.likes
     assert u.obj in c.liked
+
+
+@pytest.mark.parametrize(
+    'factory_func',
+    [
+        utils.comment.lazy_add_comment,
+        utils.comment.lazy_add_reply,
+    ],
+)
+def test_to_dict(factory_func: Callable[[], Comment]):
+    c = factory_func()
+    d = c.to_dict()
+    excepted_keys = [
+        'created',
+        'updated',
+        'submission',
+        'submissions',
+        'author',
+        'replies',
+        'liked',
+    ]
+    # check fields
+    assert all(k in d for k in excepted_keys), d.keys()
+    # TODO: validate response content
