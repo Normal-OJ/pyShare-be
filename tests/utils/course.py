19c594163339a45668b24aea48d9a2c814043e78
skps2010 <skps2010030908@yahoo.com>
2021-04-05 14:34:14 +0800

    make tags in course data optional

--- tests/utils/course.py
+++ tests/utils/course.py
@@ -1,6 +1,6 @@
 import secrets
 import random
-from typing import Union, List
+from typing import Union, List, Optional
 from mongo import *
 from mongo import engine
 from .utils import none_or
@@ -10,20 +10,21 @@ __all__ = ('data', 'lazy_add', 'Factory')
 
 
 def data(
-    name: str = None,
-    teacher: Union[str, User] = None,
-    year: int = None,
-    semester: int = None,
-    status: int = None,
-    tags: List[str] = [],
+    name: Optional[str] = None,
+    teacher: Optional[Union[str, User]] = None,
+    year: Optional[int] = None,
+    semester: Optional[int] = None,
+    status: Optional[int] = None,
+    tags: Optional[List[str]] = None,
 ):
     ret = {
         'name': none_or(name, secrets.token_hex(16)),
         'year': none_or(year, random.randint(109, 115)),
         'semester': none_or(semester, random.randint(1, 2)),
         'status': none_or(status, engine.Course.Status.PUBLIC),
-        'tags': tags,
     }
+    if tags is not None:
+        ret['tags'] = tags
     # save teacher's pk
     if teacher is not None:
         ret['teacher'] = getattr(teacher, 'pk', teacher)
