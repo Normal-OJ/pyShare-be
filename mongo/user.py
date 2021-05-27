3ef085e6b9fda7b11f80cd92474160da22fa6f3f
bogay <pojay11523@gmail.com>
2021-05-27 18:39:08 +0000

    Add `courses` field to deafule cookie

--- mongo/user.py
+++ mongo/user.py
@@ -107,6 +107,7 @@ class User(MongoBase, engine=engine.User):
             'displayName',
             'md5',
             'role',
+            'courses',
         ]
         return self.jwt(*keys)
 
