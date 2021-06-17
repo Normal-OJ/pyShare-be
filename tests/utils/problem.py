f8bfc375295fffd36010cfe5d1ce5cdb3a9ee770
bogay <pojay11523@gmail.com>
2021-06-18 00:07:09 +0800

    Support oj problem in problem test utils

--- tests/utils/problem.py
+++ tests/utils/problem.py
@@ -3,12 +3,15 @@ import random
 from typing import Optional, Union
 from mongo import *
 from . import course as course_lib
-from .utils import drop_none
+from .utils import drop_none, none_or
 
 
 def data(
     author: Optional[Union[str, User]] = None,
     course: Optional[Union[str, Course]] = None,
+    is_oj: Optional[bool] = None,
+    input: Optional[str] = None,
+    output: Optional[str] = None,
     **ks,
 ):
     if course is None:
@@ -22,17 +25,25 @@ def data(
         'author': author,
         'course': course,
     }
+    if is_oj:
+        ret.update({
+            'extra': {
+                '_cls': 'OJProblem',
+                'input': none_or(input, secrets.token_hex()),
+                'output': none_or(output, secrets.token_hex()),
+            },
+        })
     defaults = {
-        'title': lambda: secrets.token_hex(),
-        'description': lambda: secrets.token_hex(),
-        'default_code': lambda: None,
-        'tags': lambda: None,
-        'status': lambda: None,
-        'is_template': lambda: None,
-        'allow_multiple_comments': lambda: None,
+        'title': secrets.token_hex(),
+        'description': secrets.token_hex(),
+        'default_code': None,
+        'tags': None,
+        'status': None,
+        'is_template': None,
+        'allow_multiple_comments': None,
     }
     for k, v in defaults.items():
-        ret[k] = ks.get(k, v())
+        ret[k] = ks.get(k, v)
     return drop_none(ret)
 
 
