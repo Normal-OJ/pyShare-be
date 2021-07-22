09fd25931381094c051b73b55aafa50b12d38e7b
bogay <pojay11523@gmail.com>
2021-07-22 12:31:09 +0800

    Add statsd config for gunicorn
    It is commented by default because there may not exist statsd-exporter
    instanse.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,13 @@
 bind = '0.0.0.0:8080'
 timeout = 60
-
+# Log
 loglevel = 'debug'
 errorlog = 'logs/error.log'
 accesslog = 'logs/access.log'
-
+# Worker
 workers = 1
 worker_class = 'eventlet'
+# Stat
+# statsd_host = 'statsd-exporter:9125'
+# statsd_prefix = 'pyshare-web'
+
