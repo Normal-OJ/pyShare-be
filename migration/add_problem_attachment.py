a53aecd087dd6b048bb8cb3f8e2bf63aa5052b66
bogay <pojay11523@gmail.com>
2021-08-16 02:17:43 +0800

    Migration also fix submission schema

--- migration/add_problem_attachment.py
+++ migration/add_problem_attachment.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 
 client = MongoClient(os.getenv('MONGO_HOST'))
 db = client['pyShare']
+# Update problem attachments
 attachments = {}
 for problem in db['problem'].find():
     attachments[problem['_id']] = [{
@@ -15,8 +16,23 @@ for _id, attas in attachments.items():
             'attachments': attas
         }},
     )
-
+# Update submission
+db['submission'].update_many({}, {'$set': {'_cls': 'Submission'}})
+db['submission'].update_many(
+    {
+        'status': {
+            '$nin': [0, 1, 2]
+        },
+    },
+    {
+        '$set': {
+            'status': 0
+        },
+    },
+)
 # Simple validation
 from mongo import engine
 for p in engine.Problem.objects():
     p.validate()
+for s in engine.Submission.objects():
+    s.validate()
