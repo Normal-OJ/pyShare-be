4ef4539e7bd67dcc0ef8333897a26374f6b6c247
bogay <pojay11523@gmail.com>
2021-08-24 09:46:27 +0800

    Fix test: `test_course_update_tags`

--- tests/unittest/test_tag.py
+++ tests/unittest/test_tag.py
@@ -1,7 +1,14 @@
-import pytest
-from mongo import *
+import secrets
 from mongo import Tag
 from tests import utils
+
+
+def setup_function(_):
+    utils.mongo.drop_db()
+
+
+def random_tag_str():
+    return secrets.token_hex(8)
 
 
 def test_tag_add():
@@ -11,19 +18,21 @@ def test_tag_add():
 
 
 def test_tag_delete():
-    t = Tag('tag')
+    t = Tag.add('tag')
     assert len(Tag.engine.objects(value='tag')) == 1
-
     t.delete()
     assert len(Tag.engine.objects(value='tag')) == 0
 
 
 def test_tag_used_courses_count():
-    t = Tag('tag')
+    t = Tag.add('tag')
     assert t.used_courses_count() == 0
-
     c = utils.course.lazy_add(tags=['tag'])
     assert t.used_courses_count() == 1
-
     c.patch_tag([], ['tag'])
     assert t.used_courses_count() == 0
+
+
+def test_delete_tag_used_by_course():
+    tag_str = random_tag_str()
+    tag = Tag.add(tag_str)
