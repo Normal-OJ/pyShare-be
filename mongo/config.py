8d33b686cd24a00a1e8e96f4f9b75e477a1f073d
bogay <pojay11523@gmail.com>
2021-09-07 14:31:30 +0800

    Use `dynaconf` to manage config

--- mongo/config.py
+++ mongo/config.py
@@ -1,29 +1,65 @@
-__all__ = (
-    'Config',
-    'ProdConfig',
-    'ConfigLoader',
+from dynaconf import Dynaconf, Validator
+
+__all__ = ('ConfigLoader', )
+
+config = Dynaconf(
+    envvar_prefix='PYSHARE',
+    settings_files=[
+        'settings.yaml',
+        'settings.prod.yaml',
+        '.secrets.yaml',
+    ],
+    validators=[
+        Validator('ENV', default='development'),
+        #     Validator('MONGO.DB', default='pyShare'),
+        #     Validator(
+        #         'MONGO.HOST',
+        #         env='production',
+        #         must_exist=True,
+        #     ),
+        #     Validator(
+        #         'MONGO.HOST',
+        #         default='mongomock://localhost',
+        #         env='development',
+        #     ),
+        #     Validator(
+        #         'REDIS.HOST',
+        #         'REDIS.PORT',
+        #         must_exist=True,
+        #         env='production',
+        #     ),
+        #     Validator('JWT.EXP', default='30'),
+        #     Validator('JWT.ISS', default='test.test'),
+        #     # Don't use default secret under production mode
+        #     Validator(
+        #         'JWT.SECRET',
+        #         env='production',
+        #         ne='SuperSecretString',
+        #     ),
+        #     Validator(
+        #         'JWT.SECRET',
+        #         default='SuperSecretString',
+        #         env='development',
+        #     ),
+        #     Validator(
+        #         'SMTP.SERVER',
+        #         'SMTP.ADMIN',
+        #         'SMTP.ADMIN_PASSWORD',
+        #         'SMTP.NOREPLY',
+        #         'SMTP.NO_REPLY_PASSWORD',
+        #         env='production',
+        #         must_exist=True,
+        #     ),
+        # If no specified, decided by env
+        Validator('DEBUG',
+                  default=lambda setting, _: setting.ENV != 'production'),
+        Validator('TESTING',
+                  default=lambda setting, _: setting.ENV != 'production'),
+    ],
 )
 
 
-class Config:
-    TESTING = True
-    DEBUG = True
-
-
-class ProdConfig(Config):
-    TESTING = False
-    DEBUG = False
-
-
 class ConfigLoader:
-    curr_config = Config()
-
-    @classmethod
-    def load(cls, config):
-        if not issubclass(config, Config):
-            raise TypeError
-        cls.curr_config = config()
-
     @classmethod
     def get(cls, key, default=None):
-        return getattr(cls.curr_config, key, default)
+        return config.get(key, default)
