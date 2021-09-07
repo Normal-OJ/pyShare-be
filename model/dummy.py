6b9ac2b7f7efb40fb45b7dffbd1c1ab8184a52e4
bogay <pojay11523@gmail.com>
2021-09-08 00:42:32 +0800

    Test creating dummy user

--- model/dummy.py
+++ model/dummy.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from typing import Dict
 from mongo import *
 from tests import utils
 from .utils import *
@@ -6,12 +7,17 @@ from .auth import identity_verify
 
 __all__ = ['dummy_api']
 
+
+def drop_none(d: Dict):
+    return {k: v for k, v in d.items() if v is not None}
+
+
 dummy_api = Blueprint('dummy_api', __name__)
 
 
 @dummy_api.before_request
 @identity_verify(User.engine.Role.ADMIN)
-def before_dummy_api():
+def before_dummy_api(user):
     '''
     Only admin can call this
     '''
@@ -24,12 +30,12 @@ def health_check():
 
 @dummy_api.post('/user')
 @Request.json(
-    'username: str',
-    'password: str',
-    'email: str',
-    'has_email: bool',
-    'role: int',
+    'username',
+    'password',
+    'email',
+    'has_email',
+    'role',
 )
 def create_user(**ks):
-    u = utils.user.lazy_signup(**ks)
+    u = utils.user.lazy_signup(**drop_none(ks))
     return HTTPResponse(data=u.info)
--- tests/api_test/test_dummy.py
+++ tests/api_test/test_dummy.py
@@ -1,16 +1,54 @@
+import secrets
+from typing import Callable
 from flask.testing import FlaskClient
 from mongo import *
 from tests import utils
+from mongo.config import config
 
 
 class TestFakeProdEnv:
     @classmethod
     def setup_class(cls):
-        from mongo.config import config
         config.DEBUG = False
+
+    @classmethod
+    def teardown_class(cls):
+        config.DEBUG = True
 
     def test_no_dummy_api_under_prod_config(self, config_client):
         client = config_client()
         assert client.application.config['DEBUG'] == False
         rv = client.get('/dummy')
         assert rv.status_code == 404
+
+
+class TestCreateDummyResource:
+    admin = None
+
+    @classmethod
+    def setup_class(cls):
+        cls.admin = utils.user.Factory.admin()
+
+    @classmethod
+    def teardown_class(cls):
+        utils.mongo.drop_db()
+        cls.admin = None
+
+    def test_user_creation(self, forge_client: Callable[[str], FlaskClient]):
+        client = forge_client(self.admin.username)
+        username = utils.user.random_username()
+        password = secrets.token_hex()
+        email = f'{username}@pyshare.io'
+        rv = client.post(
+            '/dummy/user',
+            json={
+                'username': username,
+                'password': password,
+                'email': email,
+            },
+        )
+        assert rv.status_code == 200, rv.get_json()
+        rv_json = rv.get_json()
+        rv_user = rv_json['data']
+        assert rv_user['username'] == username
+        assert rv_user['email'] == email
