834d4c84c830f3d6d9449e48a12434526b76aeae
bogay <pojay11523@gmail.com>
2021-09-26 00:53:54 +0800

    Add some type hints for `mongo.Attachment`

--- mongo/attachment.py
+++ mongo/attachment.py
@@ -1,7 +1,8 @@
+from typing import Optional
+from datetime import datetime
 from . import engine
 from .base import MongoBase
 from .engine import GridFSProxy
-from datetime import datetime
 from .utils import doc_required
 from .user import User
 from .tag import Tag
@@ -46,7 +47,14 @@ class Attachment(MongoBase, engine=engine.Attachment):
         self.file.delete()
         self.obj.delete()
 
-    def update(self, filename, file_obj, description, patch_note, tags_str):
+    def update(
+        self,
+        filename: str,
+        file_obj,
+        description: str,
+        patch_note: str,
+        tags_str: str,
+    ):
         '''
         update an attachment from db
         '''
@@ -75,7 +83,7 @@ class Attachment(MongoBase, engine=engine.Attachment):
                     problem.author.update(push__notifs=notif.pk)
 
     @classmethod
-    def to_tag_list(cls, tags_str):
+    def to_tag_list(cls, tags_str: Optional[str]):
         if tags_str == '' or tags_str is None:
             return False
         tags = tags_str.split(',')
@@ -85,8 +93,15 @@ class Attachment(MongoBase, engine=engine.Attachment):
 
     @classmethod
     @doc_required('author', User)
-    def add(cls, author: User, file_obj, filename, description, patch_note,
-            tags_str):
+    def add(
+        cls,
+        author: User,
+        file_obj,
+        filename: str,
+        description: str,
+        patch_note: str,
+        tags_str: str,
+    ):
         '''
         add an attachment to db
         '''
