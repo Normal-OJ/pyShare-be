c9cf3c72f07fef7b0aa470632db43360f47fcf1b
skps2010 <skps2010030908@yahoo.com>
2021-10-22 17:06:30 +0800

    add migration

---
+++ migration/change_to_acceptance.py
@@ -0,0 +1,35 @@
+from mongo.config import config
+from pymongo import MongoClient
+
+client = MongoClient(config['MONGO']['HOST'])
+db = client[config['MONGO']['DB']]
+
+for comment in db['comment'].find():
+    submission_ids = comment['submissions']
+    if len(submission_ids) == 0:
+        acceptance = 3  # NOT_TRY
+    else:
+        acceptance = 2  # PENDING
+        for id in submission_ids:
+            submission = db['submission'].find({'_id': id})
+            if submission['state'] == 1:  # ACCEPT
+                acceptance = 0  # ACCEPTED
+                break
+            if submission['state'] == 2:  # DENIED
+                acceptance = 1  # REJECTED
+    db['comment'].update_one(
+        {'_id': comment['_id']},
+        {
+            '$unset': {
+                'hasAccepted': ''
+            },
+            '$set': {
+                'acceptance': acceptance
+            }
+        },
+    )
+
+# Simple validation
+from mongo import engine
+for c in engine.Comment.objects():
+    c.validate()
