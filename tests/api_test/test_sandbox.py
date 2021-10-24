b8e9226b757a5d102fe0c427f3a9a3d716a66a58
bogay <pojay11523@gmail.com>
2021-10-24 18:09:48 +0800

    test(sandbox): test we cannot update existent sandbox's token

--- tests/api_test/test_sandbox.py
+++ tests/api_test/test_sandbox.py
@@ -87,3 +87,25 @@ def test_update_sandbox(forge_client: Callable[[str, Optional[str]],
     assert rv.status_code == 200
     sandbox = engine.Sandbox.objects(url=url).get()
     assert sandbox.alias == 'sandbox'
+
+
+def test_cannot_update_sandbox_token(
+        forge_client: Callable[[str, Optional[str]], FlaskClient]):
+    url = 'http://test.sandbox'
+    engine.Sandbox(
+        url=url,
+        token=secrets.token_urlsafe(),
+    ).save(force_insert=True)
+    admin = utils.user.Factory.admin()
+    client = forge_client(admin.username)
+    new_token = secrets.token_urlsafe()
+    rv = client.put(
+        '/sandbox',
+        json={
+            'url': url,
+            'token': new_token,
+        },
+    )
+    assert rv.status_code == 200
+    sandbox = engine.Sandbox.objects(url=url).get()
+    assert sandbox.token != new_token
