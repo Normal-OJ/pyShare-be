4d62a404a48f96cbdc831bf522bb14c3485ccbeb
bogay <pojay11523@gmail.com>
2021-12-05 20:23:25 +0000

    test(SolveOJProblem): result of AC/WA submission

---
+++ tests/unittest/requirement/test_solve_oj_problem.py
@@ -0,0 +1,57 @@
+from datetime import datetime, timedelta
+from tests import utils
+from mongo.sandbox import ISandbox
+from mongo.task import Task
+from mongo import requirement
+
+
+def setup_function(_):
+    ISandbox.use(utils.submission.MockSandbox)
+
+
+def teardown_function(_):
+    ISandbox.use(None)
+    utils.mongo.drop_db()
+
+
+# TODO: Simplify setup procedure
+
+
+def test_solve_oj_problem_can_count_AC_submission():
+    problem = utils.problem.lazy_add(is_oj=True)
+    task = Task.add(
+        course=problem.course,
+        ends_at=datetime.now() + timedelta(minutes=5),
+    )
+    req = requirement.SolveOJProblem.add(
+        task=task,
+        problems=[problem],
+    )
+    submission = utils.submission.lazy_add_new(problem=problem)
+    submission.complete(
+        files=[],
+        stdout='',
+        stderr='',
+        judge_result=submission.JudgeResult.AC,
+    )
+    assert req.reload().is_completed(submission.user)
+
+
+def test_solve_oj_problem_wont_be_triggerd_by_WA_submission():
+    problem = utils.problem.lazy_add(is_oj=True)
+    task = Task.add(
+        course=problem.course,
+        ends_at=datetime.now() + timedelta(minutes=5),
+    )
+    req = requirement.SolveOJProblem.add(
+        task=task,
+        problems=[problem],
+    )
+    submission = utils.submission.lazy_add_new(problem=problem)
+    submission.complete(
+        files=[],
+        stdout='',
+        stderr='',
+        judge_result=submission.JudgeResult.WA,
+    )
+    assert not req.reload().is_completed(submission.user)
